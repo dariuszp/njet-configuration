caa5163aad531be6162ec22caebc285e8a4b0dcc
Półtorak <poltorak.dariusz@gmail.com>
2014-04-05 22:12:16 +0200

    load file tests

--- src/loader/directory.js
+++ src/loader/directory.js
@@ -1,9 +1,33 @@
 'use strict';
 
-var fileLoader = require(__dirname + '/file.js');
+var yamlLoader      = require(__dirname + '/file.js'),
+    fs              = require('fs');
+
+function getYamlName(filename) {
+    filename = String(filename);
+    return (filename.length > 4 && filename.substr(-4).toLowerCase() === '.yml') ? filename.substr(-4) : false;
+}
 
 module.exports = {
     load: function (path, encoding) {
+        if (!fs.existsSync(path)) {
+            return false;
+        }
+        path = fs.realpathSync(path);
+        if (!path) {
+            return false;
+        }
+        var files = fs.readdirSync(path),
+            i,
+            result = {},
+            name;
 
+        for (i = 0; i < files.length; i++) {
+            if ((name = getYamlName(files[i])) !== false) {
+                result[name] = yamlLoader.load(path + files[i]);
+            }
+        }
+
+        return result;
     }
 };
--- src/loader/file.js
+++ src/loader/file.js
@@ -5,17 +5,6 @@ var yaml = require('js-yaml'),
 
 module.exports = {
     load: function (path, encoding) {
-        if (!fs.existsSync(path)) {
-            return false;
-        }
-        try {
-            var doc = yaml.safeLoad(fs.readFileSync(path, encoding || 'utf8'));
-            if (!(doc instanceof Object)) {
-                doc = false;
-            }
-            return doc;
-        } catch (e) {
-            return false;
-        }
+        return yaml.safeLoad(fs.readFileSync(path, encoding || 'utf8'));
     }
 };
